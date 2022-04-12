beed0458dab28a8be747584106b673f9dbf99a4f
zapikankaa <tugaevad@gmail.com>
2022-04-12 23:45:19 +0500

    Revert "Deleted default view Home"
    This reverts commit b574e4249a989b988b455798a2b61e37d508d448.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Main from '../views/Main'
-import Question2 from '../views/Question2'
-import Question3 from '../views/Question3'
-import Question4 from '../views/Question4'
-import Question5 from '../views/Question5'
-import Prediction from '../views/Prediction'
+import Question2 from '../components/Question2'
+import Question3 from '../components/Question3'
+import Question4 from '../components/Question4'
+import Question5 from '../components/Question5'
+import Prediction from '../components/Prediction'
 
 const routes = [
   {
